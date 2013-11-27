487a023911256746f466adba13d66b8d78c1f075
Joanna Lindamulage <jenyjl25@gmail.com>
2013-11-26 20:12:13 -0800

    Initial Commit

---
+++ Test3/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Test3
+//
+//  Created by Joanna Lindamulage on 11/26/13.
+//  Copyright (c) 2013 Joanna Lindamulage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Test3/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Test3
+//
+//  Created by Joanna Lindamulage on 11/26/13.
+//  Copyright (c) 2013 Joanna Lindamulage. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
